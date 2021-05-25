913e290e0d00b14b69795f48cbe74706e271e6e7
rahulbasani <59694236+rahulbasani@users.noreply.github.com>
2021-05-25 16:49:47 +0530

    added subnet

--- vpc.tf
+++ vpc.tf
@@ -9,3 +9,12 @@ resource "aws_vpc" "main" {
     Name = "main"
   }
 }
+
+resource "aws_subnet" "main_subnet"{
+  vpc_id = aws_vpc.main.id
+  cidr_block="10.0.10.0/24"
+  
+  tags ={
+    Name = "main_subnet"
+  }
+}
