609245642628bd3ffcef40695ab32521e152c6c3
rahulbasani <59694236+rahulbasani@users.noreply.github.com>
2021-05-31 14:20:22 +0530

    update state files to s3

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,11 @@
 provider "aws"{
   region = "ap-southeast-1"
 }
+backend "s3" {
+  bucket = "bucketforstatefiles"
+  key = "terraform.tfstatefiles"
+  region = "ap-southeast-1"
+}
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
