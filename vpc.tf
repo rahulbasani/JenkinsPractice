97c6115d84b359c37ce0db727a1645a5d13ef1b1
rahulbasani <59694236+rahulbasani@users.noreply.github.com>
2021-05-31 14:23:18 +0530

    changes

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,12 @@
 provider "aws"{
   region = "ap-southeast-1"
 }
-backend "s3" {
-  bucket = "bucketforstatefiles"
-  key = "terraform.tfstatefiles"
-  region = "ap-southeast-1"
+terraform{
+  backend "s3" {
+    bucket = "bucketforstatefiles"
+    key = "terraform.tfstatefiles"
+    region = "ap-southeast-1"
+  }
 }
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
